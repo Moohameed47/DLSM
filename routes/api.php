64b11f583613d2eb1e1c857d0890d7134b8aea3c
Moohameed47 <d.mohamedtarek12@gmail.com>
2024-02-13 12:18:16 +0200

    API-Route Part1

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\agentController;
 use App\Http\Controllers\clientController;
+use App\Http\Controllers\shipping_companyController;
 use App\Http\Controllers\logoutController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\registerController;
+use App\Http\Controllers\fac_ex_im_companyController;
 use App\Http\Controllers\profileController;
+use App\Http\Controllers\offerController;
+use App\Http\Controllers\requestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,19 +29,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('admins', [adminController::class, 'index'])->name('admin');
+//Get All Data From DB
+Route::get('admins', [adminController::class, 'index']);
+Route::get('clients', [clientController::class, 'index']);
+Route::get('agents', [agentController::class, 'index']);
+Route::get('shipping_companies', [shipping_companyController::class, 'index']);
+Route::get('fac_ex_im_companies',[fac_ex_im_companyController::class,'index']);
+Route::get('offers', [offerController::class, 'index']);
+Route::get('requests', [requestController::class, 'index']);
+
+//Login & Register
 Route::post('register',[registerController::class,'register']);
 Route::post('login',[loginController::class,'login']);
-Route::get('clients', [clientController::class, 'index'])->name('client');
-Route::get('agents', [agentController::class, 'index'])->name('agent');
-Route::get('agents/{id}', [agentController::class, 'show'])->name('agents.show');
-Route::get('agents/destroy/{id}', [agentController::class, 'delete'])->name('agents.delete');
-Route::post('agents-create', [agentController::class, 'store'])->name('agents.create');
-Route::group(
-    [   
-        "middleware" => ["auth:sanctum"]
-    ],function() {
-        Route::get("profile",[profileController::class,"profile"]);
-        Route::get("logout",[logoutController::class,"logout"]);
-    }
-);
+
+//Agents
+Route::get('agents/{id}', [agentController::class, 'show']);
+Route::get('agents/destroy/{id}', [agentController::class, 'delete']);
+Route::post('agents-create', [agentController::class, 'store']);
+
